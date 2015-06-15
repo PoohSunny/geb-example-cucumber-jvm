786f20dcaab532aa6a2160e2b75fcb26b60d7580
youtarot <youtarot@h-t.co.jp>
2015-06-15 23:16:56 +0900

    ステップの追加

--- src/cucumber/resources/steps/event_list_steps.groovy
+++ src/cucumber/resources/steps/event_list_steps.groovy
@@ -10,3 +10,13 @@ import static cucumber.api.groovy.JA.*
 	at EventsPage
 	assert page.events.size() > 1
 }
+
+もし(~/^表示されている一番上のイベントの情報をクリックする$/) { ->
+	// Write code here that turns the phrase above into concrete actions
+	throw new PendingException()
+}
+
+ならば(~/^イベントの詳細ページが表示される$/) { ->
+	// Write code here that turns the phrase above into concrete actions
+	throw new PendingException()
+}
